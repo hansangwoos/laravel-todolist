72d554e18400b5a15f33d4384573e5c679ea35c2
hansangwoo <hansangwoos456@gmail.com>
2025-08-23 18:39:08 +0900

    feat : 등록한 todo table 화이트 리스트 속성 등록 및 데이터 타입 캐스팅 등록

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    // Mass Assignment 보호를 위한 fillable 설정
+    protected $fillable =[
+        'title',
+        'description',
+        'is_completed',
+    ];
+
+    protected $casts = [
+        'is_completed' => 'boolean'
+    ];
 }
