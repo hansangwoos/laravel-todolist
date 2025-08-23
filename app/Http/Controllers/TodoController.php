d233e1abb9d7f93a167538893caff652b1315a15
hansangwoo <hansangwoos456@gmail.com>
2025-08-23 18:40:01 +0900

    teat : 기본 Controller  생성 및 index() 구현 - 모든 할 일 가져오기 - 최신순으로 가져오기 - 완료되지않은 일 가져오기

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // 모든 할일 가져오기
+        $todos = Todo::all();
+
+        // 최신순으로 가져오기
+        // $todos = Todo::latest()->get();
+
+        // 완료되지않은 일만 가져오기
+        // $todos = Todo::where('is_completed', false)->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
